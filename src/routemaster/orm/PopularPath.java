4a8c8d37c2cdfa433f45187f131de490e51fa79c
Lauren Frizzell <lefrizzell@gmail.com>
2013-02-19 13:59:33 -0500

    worked on classes
    some need attention because I was unsure of data types

--- src/routemaster/orm/PopularPath.java
+++ src/routemaster/orm/PopularPath.java
@@ -5,8 +5,34 @@ import com.mongodb.BasicDBObject;
 import java.util.Date;
 
 public PopularPath implements IDocument {
+	//Start and end geographic positions (order doesn’t matter)
+	private start;
+	private end;
+	//References to associated routes (using MongoDB ObjectIDs)
+	private Route route;
+	
+	public PopularPath(DBObject base) {
+		
+		
+	}
+	
+	public PopularPath() {
+		
+		
+	}
+	
+	//get
+	//set
 	
 	
-	
-	
+    public DBObject toDBObject() {
+        return new BasicDBObject().
+            append("ts", ts).
+            append("lt", lt).
+            append("lg", lg).
+            append("al", al).
+            append("ac", ac).
+            append("ui", ui).
+            append("rc", rc);
+    }
 }
--- src/routemaster/orm/Waypoint.java
+++ src/routemaster/orm/Waypoint.java
@@ -4,10 +4,73 @@ import com.mongodb.DBObject;
 import com.mongodb.BasicDBObject;
 import java.util.Date;
 
+//waypoints (2 letter names)
 public Waypoint implements IDocument {
+	//Timestamp
+	private Date ts;
+	//Geographic position (latitude, longitude, and altitude)
+	//what data type would this be?
+	private lt; //lat
+	private lg; //long
+	private al; //alt
+	//Measurement accuracy (reported by the user’s device)
+	private float ac;
+	//Associated route (using MongoDB ObjectIDs)
+	private rt;
+	//Associated short user id
+	private int ui;
 	
+	public Waypoint(DBObject base) {
+        ts = (Date)base.get("ts");
+        lt = ()base.get("lt");
+        lg = ()base.get("lg");
+        al = ()base.get("al");
+        ac = ((Number)base.get("ac")).floatValue();
+        rt = ()base.get("rt");
+        ui = ((Number)base.get("ui")).intValue();
+	}
 	
+	public Waypoint(int ui) {
+        this.ui = ui;
+        ts = new Date();      
+	}
 	
+	public Date getTs() {
+		return ts;
+	}
 	
+	public __ getLt() {
+		return lt;
+	}
 	
+	public __ getLg() {
+		return lg;
+	}
+	
+	public __ getAl() {
+		return al;
+	}
+	
+	public flaot getAc() {
+		return ac;
+	}
+	
+	public __ getRt() {
+		return rt;
+	}
+	
+	public int getUi() {
+		return ui;
+	}
+	
+    public DBObject toDBObject() {
+        return new BasicDBObject().
+            append("ts", ts).
+            append("lt", lt).
+            append("lg", lg).
+            append("al", al).
+            append("ac", ac).
+            append("ui", ui).
+            append("rc", rc);
+    }
 }
