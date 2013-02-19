c49f52a308eaccd14f863bccd6fa6a1a9de12a0d
Lauren Frizzell <lefrizzell@gmail.com>
2013-02-19 10:51:40 -0500

    started Session class
    one question about the documentation, it refers to a short uid,
    this is still an int, right? Not an actual short data type?
    I feel like this is a stupid question but alas, it must be
    asked

---
+++ src/routemaster/orm/Session.java
@@ -0,0 +1,46 @@
+package routemaster.orm;
+
+import com.mongodb.DBObject;
+import com.mongodb.BasicDBObject;
+import java.util.Date;
+
+public Session implements IDocument() {
+	//Timestamp
+	private timestamp;
+	//Long session UUID number
+	private long uuid;
+	//Associated short user id
+	private int uid;
+	
+    public Session(DBObject base) {
+        uuid = ((Number)base.get("uuid")).longValue();
+        uid = ((Number)base.get("uid")).intValue();
+        timestamp = (Date)base.get("timestamp");
+    }
+	
+    //for new session
+    public Session(long uuid, int uid) {
+    	this.uuid = uuid;
+    	this.uid = uid;
+    	timestamp = new Date();
+    }
+	
+	getUuid() {
+		return uuid;
+	}
+	
+	getUid() {
+		return uid;
+	}
+	
+	getTimestamp() {
+		return timestamp;
+	}	
+	
+    public DBObject toDBObject() {
+        return new BasicDBObject().
+            append("uuid", uuid).
+            append("uid", uid).
+            append("timestamp", timestamp);
+    }
+}
