a7e64b2c120893d7847e5fd19d0b09090d38cf45
Colin Chan <colinchan@lumeh.org>
2013-03-17 18:43:59 -0400

    Write the request URI to output

--- src/routemaster/MyHandler.java
+++ src/routemaster/MyHandler.java
@@ -1,21 +1,25 @@
 package routemaster;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import org.eclipse.jetty.http.HttpURI;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.Request;
 
 public class MyHandler extends AbstractHandler {
-
     public void handle(String target, Request baseRequest,
         HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException
     {
-        response.setContentType("text/plain");
+        response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
-        response.getWriter().println("<h1>Hello OneHandler</h1>");
+        PrintWriter out = response.getWriter();
+        out.println("Hello y'all!");
+        HttpURI uri = baseRequest.getUri();
+        out.println("The uri you requested was: " + uri.getDecodedPath());
     }
 }
