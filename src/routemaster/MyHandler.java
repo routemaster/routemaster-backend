7b0476d0d7781b2f3730bef89744eaa3d9739c2e
Colin Chan <colinchan@lumeh.org>
2013-03-17 19:09:14 -0400

    Attempt to parse the uri and return user info

--- src/routemaster/MyHandler.java
+++ src/routemaster/MyHandler.java
@@ -9,6 +9,12 @@ import org.eclipse.jetty.http.HttpURI;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.Request;
 
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import routemaster.jrm.ConvertToJSON;
+import routemaster.jrm.DBFunctions;
+import routemaster.orm.User;
+
 public class MyHandler extends AbstractHandler {
     public void handle(String target, Request baseRequest,
         HttpServletRequest request, HttpServletResponse response)
@@ -18,8 +24,24 @@ public class MyHandler extends AbstractHandler {
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
         PrintWriter out = response.getWriter();
-        out.println("Hello y'all!");
         HttpURI uri = baseRequest.getUri();
-        out.println("The uri you requested was: " + uri.getDecodedPath());
+        String path = uri.getDecodedPath();
+        // Parse the URI
+        if (path.startsWith("/user/")) {
+            int userid = Integer.parseInt(path.substring(6));
+            // Get a user from the database
+            DBFunctions dbf = new DBFunctions();
+            DBCursor db = dbf.getUserFromDB(userid);
+            DBObject dbobj = db.next();
+            User user = new User(dbobj);
+            ConvertToJSON ctj = new ConvertToJSON();
+            String userJSON = ctj.convertUser(user).toString();
+            out.println(userJSON);
+        }
+        else {
+            out.println("Hello y'all!");
+            out.println("The uri you requested was: " + path);
+            out.println("Try requesting a uri like /user/3");
+        }
     }
 }
