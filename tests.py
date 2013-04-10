09271185dded232fc9c3eeb119423453444ba9dc
sternJosh <bush@ufl.edu>
2013-04-10 18:58:54 -0400

    created tests for adding routes and waypoints

--- tests.py
+++ tests.py
@@ -18,6 +18,24 @@ class RoutemasterTestCase(unittest.TestCase):
         user = r.json()
         r2 = requests.get(SERVER+'/user/{}/'.format(user['id']))
         assert 'Colin Chan' in r2.text
+   
+    def test_put_route(self):
+        walk = {'date': date.today(), 'distance': 100, 'disqualified': False, 'efficiency': 5, 'time': datetime.now()}
+        r = requests.post(SERVER+'/route/', data=walk)
+        assert r.status_code == 200
+        testRoute = r.json()
+        r2 = requests.get(SERVER+'/route/{}/'.format(testRoute['id']))
+        dist = 100
+        assert dist in r2.text
+
+    def test_put_waypoint(self):
+        point = {'name': 'newPoint', 'date': date.today(), 'accuracy': '4.3', 'latitude': '50.23', 'longitude': '23.45'}
+        r = requests.post(SERVER+'/waypoint/', data=point)
+        assert r.status_code == 200
+        testPoint = r.json()
+	r2 = requests.get(SERVER+'/waypoint/{}/'.format(testPoint['id']))
+	acc = 4.3
+	assert acc in r2.text
 
 if __name__ == '__main__':
     results = unittest.main(exit=False).result
